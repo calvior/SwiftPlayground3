58fdccbb44e35c9bc64f298f1944a44798b25717
CM Student <cmstudent@UHWO-ACM-08.local>
2018-02-12 10:18:59 -1000

    finish SP3 in class portion

--- SwiftPlayground3.playground/Contents.swift
+++ SwiftPlayground3.playground/Contents.swift
@@ -3,3 +3,34 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//First decalre a variable as array using []
+
+var fruitBasket: [String]
+
+
+//VS simple string variable; w/o []
+
+var simpleString: String
+
+
+//Initialize an array using an array literal
+
+fruitBasket = ["Watermelons", "Plums", "Blackberries", "Peaches"]
+print(fruitBasket)
+
+
+//The for-in loop
+
+for name in fruitBasket{
+//print "I like to eat" before every name in fruitBasket
+    print("I like to eat " + name)
+}
+
+//Let's print out item at index 1
+
+print(fruitBasket[1])
+
+//print out item at index 0
+
+print(fruitBasket[0])
